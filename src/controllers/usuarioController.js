f5046944f47d1c42439be8857f63e9496b7ceedf
YasminEloitaNascimento <yasmin.nascimento@sptech.school>
2026-08-26 22:18:29 -0300

    feat: atualização do controller e model para conexão com db

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,9 +1,8 @@
 var usuarioModel = require("../models/usuarioModel");
-var aquarioModel = require("../models/aquarioModel");
 
 function autenticar(req, res) {
     var email = req.body.emailServer;
-    var senha = req.body.senhaServer;
+    var senha = req.body.passwordHashServer;
 
     if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
@@ -15,25 +14,18 @@ function autenticar(req, res) {
             .then(
                 function (resultadoAutenticar) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
-                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`);
 
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
 
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
+                        res.json({
+                            id: resultadoAutenticar[0].id_usuario, 
+                            email: resultadoAutenticar[0].email,
+                            name: resultadoAutenticar[0].nome,     
+                            cargo: resultadoAutenticar[0].cargo
+                        });
+
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
@@ -48,29 +40,28 @@ function autenticar(req, res) {
                 }
             );
     }
-
 }
 
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-    var nome = req.body.nomeServer;
+    var nome = req.body.nameServer;
     var email = req.body.emailServer;
-    var senha = req.body.senhaServer;
-    var fkEmpresa = req.body.idEmpresaVincularServer;
+    var senha = req.body.passwordHashServer;
+    var token = req.body.tokenServer;
 
     // Faça as validações dos valores
-    if (nome == undefined) {
-        res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
-    } else if (senha == undefined) {
-        res.status(400).send("Sua senha está undefined!");
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
+    if (!nome) {
+        res.status(400).send("O nome está indefinido!");
+    } else if (!email) {
+        res.status(400).send("O e-mail está indefinido!");
+    } else if (!senha) {
+        res.status(400).send("A senha está indefinida!");
+    } else if (!token) {
+        res.status(400).send("O token está indefinido!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
+        usuarioModel.cadastrar(nome, email, senha, token)
             .then(
                 function (resultado) {
                     res.json(resultado);
